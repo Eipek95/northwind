2f1ed577a7e90e9bc0b875f64795a428832e1ec3
Eipek95 <eipek1995@gmail.com>
2022-09-02 18:28:45 +0300

    add category/product

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,10 +1,7 @@
-export interface Product{ //export--->public typescript : veri güvenliğini sağlar
-    productId:number;
-    categoryId:number;
-    productName:string;
-    unitsInStock:number;
-    unitPrice:number
+export interface Product {
+  productId: number;
+  categoryId: number;
+  productName: string;
+  unitsInStock: number;
+  unitPrice: number;
 }
-
-
-//c# taki dto gibi classı gibi düşün
