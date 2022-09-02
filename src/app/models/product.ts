a0a00bd4017b14223d651c09ade9a5f0b7e22bec
Eipek95 <eipek1995@gmail.com>
2022-09-02 14:59:01 +0300

    first commit

---
+++ src/app/models/product.ts
@@ -0,0 +1,10 @@
+export interface Product{ //export--->public typescript : veri güvenliğini sağlar
+    productId:number;
+    categoryId:number;
+    productName:string;
+    unitsInStock:number;
+    unitPrice:number
+}
+
+
+//c# taki dto gibi classı gibi düşün
